4570eb0d1680ac7e28d80c96106fd83a7705c9a5
Felipe Cortez <felipecortezfi@gmail.com>
2019-11-21 17:11:15 -0300

    Also rewrite project.clj before checking if the linter worked

--- cljfmt/entrypoint.sh
+++ cljfmt/entrypoint.sh
@@ -5,18 +5,26 @@ set -euo pipefail
 # shellcheck disable=SC1091
 source /lib.sh
 
+rewrite() {
+  clojure -Sdeps '{:deps {rewrite-clj {:mvn/version "0.6.1"}}}' /rewrite_projectclj.clj
+}
+
 cljfmt() {
+  rewrite
   lein cljfmt "$@"
+  git checkout -- project.clj
 }
 
 fix() {
+  rewrite
   lein cljfmt fix
   git checkout -- project.clj
 }
 
 lint() {
+  rewrite
 	lein cljfmt check
+  git checkout -- project.clj
 }
 
-clojure -Sdeps '{:deps {rewrite-clj {:mvn/version "0.6.1"}}}' /rewrite_projectclj.clj
 _lint_and_fix_action cljfmt "${@}"
