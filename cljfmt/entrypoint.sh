8fac4c22cbf0a1a8c33f2499caad137ef0e08a9b
Felipe Cortez <felipecortezfi@gmail.com>
2019-11-21 16:54:40 -0300

    Add lots of debug statements

--- cljfmt/entrypoint.sh
+++ cljfmt/entrypoint.sh
@@ -18,6 +18,10 @@ lint() {
 }
 
 echo $(pwd)
+echo "trying to rewrite project.clj"
+cat project.clj
 clojure -Sdeps '{:deps {rewrite-clj {:mvn/version "0.6.1"}}}' /rewrite_projectclj.clj
+echo "was it rewritten?"
+cat project.clj
 git checkout -- project.clj
 _lint_and_fix_action cljfmt "${@}"
